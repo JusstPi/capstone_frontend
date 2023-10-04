4f8eb919294240b5ef4cdb548196f807c564b29f
Joann Alfante <joann.alfante@cit.edu>
2023-10-04 11:26:04 +0800

    Update App.js
    update app.js lang

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 
 import Calendar from './pages/calendar_page';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import MyReservations from './pages/my_reservations';
-import UserMyReservations from './pages/my_reservations/userindex';
+import UserMyReservations from './pages/my_reservations';
 
 
 function App() {
   return (
     <div className="App" >
       <Calendar />
-        
-      
     </div>
   );
 }
